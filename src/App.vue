2421619ca4272726654244961fc7437275e53295
Katanon Krengsomrong <66160196@go.buu.ac.th>
2024-12-03 00:58:24 +0700

    Finish MyBmi

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,12 @@ import ReactiveFundamentals from './components/ReactivityFundamentals.vue'
 import FormInputBindings from './components/FormInputBindings.vue'
 import HelloMyName from './components/HelloMyName.vue'
 import MyPlus from './components/MyPlus.vue'
+import MyBmi from './components/MyBmi.vue'
 </script>
 
 <template>
+  <MyBmi></MyBmi>
+  <hr />
   <MyPlus></MyPlus>
   <hr />
   <HelloMyName></HelloMyName>
---
+++ src/components/MyBmi.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <label for="weight"> น้ำหนัก</label>
+    <input type="text" id="weight" v-model.number="weight" /> kg. <br>
+    <label for="height"> ส่วนสูง</label>
+    <input type="text" id="height" v-model.number="height" /> cm. <br>
+    {{ weight }}
+    {{ height }}
+    <br>
+    <button @click="calBmi">Cal BMI</button>
+    <br>
+    <label> BMI =</label>
+    {{ result }}
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const height = ref(0)
+const weight = ref(0)
+const result = ref(0)
+function calBmi() {
+  result.value = weight.value / Math.pow(height.value/100,2)
+}
+</script>
