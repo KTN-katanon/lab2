d34f3d2837c38c4d2050611ed7a0187ad47332d5
Katanon Krengsomrong <66160196@go.buu.ac.th>
2024-12-02 23:36:15 +0700

    Finish Reactive 1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import Hello from './components/HelloComponent.vue'
 import TemplateSyntax from './components/TemplateSyntax.vue'
+import ReactiveFundamentals from './components/ReactivityFundamentals.vue'
 </script>
 
 <template>
-<div>Hello</div>
-<span><Hello></Hello></span>
-<TemplateSyntax></TemplateSyntax>
+  <ReactiveFundamentals></ReactiveFundamentals>
+  <hr />
+  <div>Hello</div>
+  <hr />
+  <span><Hello></Hello></span>
+  <hr />
+  <TemplateSyntax></TemplateSyntax>
+  <hr />
 </template>
 
 <style scoped>
