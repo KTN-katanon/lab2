4053cddfdc1a5e940ae67aed883030ed17f36c89
Katanon Krengsomrong <66160196@go.buu.ac.th>
2024-12-02 23:19:22 +0700

    Update TemplateSyntax.vue
    Finish Binding 1

--- src/components/TemplateSyntax.vue
+++ src/components/TemplateSyntax.vue
@@ -1,7 +1,14 @@
 <script setup>
-
+var msg = 'Hello Message'
+var rawHtml = '<h1>Hello</h1>'
+var myId ='myId1'
+var color = 'blue'
 </script>
 
 <template>
-  <div></div>
+  <div>{{ msg }}</div>
+  <div v-html="rawHtml"></div>
+  <div :id="myId"></div>
+  <font :color="color">Red</font>
+  <button :disabled="isDisable">isEnable</button>
 </template>
